d6670e569550b6df2e4ef5d024722e96d4a566c6
jihyuno301 <joh755807@gmail.com>
2019-03-31 20:42:10 +0900

    vue파일

---
+++ your-component.vue
@@ -0,0 +1,22 @@
+<template lang="html"> <!--화면에 표시할 요소들을 정의하는 영역 ex)HTML+Vue data binding-->
+  <div>
+    <span>
+      <button type="button" name="button"> {{message}}</button>
+    </span>
+  </div>
+</template>
+
+<script> //뷰 컴포넌트 내용을 정의하는 영역 ex)template, data, methods
+export default {
+  data : {
+    message : 'click this button'
+  }
+}
+</script>
+
+<!--템플릿에 추가한 HTML태그의 CSS스타일을 정의하는 영역-->
+<style lang="css" scoped>
+  span {
+    font-size: 1.2em;
+  }
+</style>
