33d7df67d81f1556677a3e5cb10d31b91510dd5b
jihyuno301 <joh755807@gmail.com>
2019-05-09 16:08:59 +0900

    single, multiple slot

---
+++ slot/App.vue
@@ -0,0 +1,23 @@
+<!--props를 통해 parent에서 child로 데이터를 전달하였는데 이보다 큰 컨텐츠(html코드 덩어리)를  넘기고자 할 때
+slot을 이용한다.-->
+<!-- 특정 모양을 가진 공통프레임에 컨텐츠만 다르게 들어갈 component를 만들 때 유용하다. -->
+<!-- App.vue - parent-->
+<template>
+    <div>
+      <app-quote>
+        <h1 slot="title">The Quote</h1>
+        <p slot="content">Hello world!</p>
+      </app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from 'Quote.vue';
+
+export default {
+  components : { appQuote : Quote }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ slot/Quote.vue
@@ -0,0 +1,19 @@
+<!--Quote.vue - child-->
+<template>
+  <div>
+    <slot name="title"></slot> <!--이 부분에 <h1>The Quote</h1><p>Hello world!</p>가 온다.-->
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<!-- slot으로 넘기는 코드들은 컴파일되어 전달된다. 따라서 모든 Control은 parent component에서 진행된다. -->
+<!--
+multiple slots : vue.jssms unnamed slot을 default slot으로 다룬다.
+slot에 이름을 주고, slot attribute를 이용한다.
+-->
+<style lang="css" scoped>
+</style>
