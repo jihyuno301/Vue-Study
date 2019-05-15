7e35d083c06d8142406f5df0fcd37d8543352299
jihyuno301 <joh755807@gmail.com>
2019-05-15 13:03:17 +0900

    css flex element 연습

---
+++ css-flex-prac.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="parent">
+      <div class="child">
+        <ProblemType></ProblemType>
+      </div>
+      <div class="child">
+        <ProblemType></ProblemType>
+      </div>
+      <div class="child">
+        <ProblemType></ProblemType>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import ProblemType from './sample/lecture_type/problem__type.vue'
+
+export default {
+  components : {
+    'ProblemType' : ProblemType,
+
+  }
+}
+
+</script>
+
+
+<style lang="less">
+#app {
+  margin-top: 1.5rem;
+  line-height: 1.4285;
+}
+.parent {
+  display : flex;
+  flex-direction : row;
+}
+.child {
+  flex : 1 1 50px;
+}
+</style>
